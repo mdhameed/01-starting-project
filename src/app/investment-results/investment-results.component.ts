5363507b1ab5437b49f631f3a4973d57ca7d88ec
Mohammed Hameed <hameed.in@hotmail.com>
2024-08-21 23:17:49 +0530

    Add a Service and use signals.

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,6 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -9,12 +10,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input() results?: {
-    year: number,
-    interest: number,
-    valueEndOfYear: number,
-    annualInvestment: number,
-    totalInterest: number,
-    totalAmountInvested: number
-  }[];
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultData());
 }
